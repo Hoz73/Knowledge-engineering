d772984164a38ae14268d2902d3618c757b7838f
Hamze <hamzaalrashed1@hotmail.com>
2021-12-06 10:25:15 +0100

    started to define the classes and types

---
+++ classes.py
@@ -0,0 +1,82 @@
+from enum import Enum, unique
+
+
+@unique
+class Type(Enum):
+    RDFS = 0
+    RDF = 1
+    OWL = 2
+    FOAF = 3
+    SKOS = 4
+    DC = 5
+
+
+##################################### knowledge engineering ####################################
+@unique
+class RDFS(Enum):
+    subClassOf = 0
+    range = 1
+    domain = 2
+    subPropertyOf = 3
+    label = 4
+
+
+@unique
+class SKOS(Enum):
+    # SKOS Properties
+    member = 0
+    collection = 1
+    prefLabel = 2
+    related = 3
+    hiddenLabel = 4
+    broader = 5
+    narrower = 6
+    definition = 7
+    altLabel = 8
+    scopeNot = 9
+
+    # SKOS Classes
+    Concept = 10
+
+
+
+@unique
+class EDM(Enum):
+    # EDM Classes
+    Agent = 0
+    ProvidedCHO = 1
+
+    # EDM Properties
+
+
+@unique
+class OWL(Enum):
+    pass
+
+
+########################################## semantic web #####################################
+@unique
+class RDF(Enum):
+    type = 0
+    Property = 1
+    first = 2
+
+
+@unique
+class FOAF(Enum):
+    pass
+
+
+@unique
+class DC(Enum):
+    pass
+
+
+#################################################################################################
+
+class Relation:
+    pass
+
+
+class Node:
+    pass
